61754a46b83d59029fe972008f67371e7a3b8ffc
Bassline32 <ivandzen911@Gmail.com>
2025-12-12 16:14:04 +0300

    Таска 2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
+        Solution2 solution2 = new Solution2();
+        int[] list1 = new int[]{};
+        int[] list2 = new int[]{0};
+        int[]  returnMiltilist = solution2.mergeLists(list1, list2);
+
+        System.out.println(Arrays.toString(returnMiltilist));
+
     }
 }
 
--- src/Solution2.java
+++ src/Solution2.java
@@ -16,11 +16,10 @@ Input: list1 = [], list2 = [0]
 Output: [0]
 */
 
+import java.util.Arrays;
+
 public class Solution2 {
     public int[] mergeLists(int[] list1, int[] list2) {
-        //задал массивы
-        list1 = new int[]{2, 4, 5, 8};
-        list2 = new int[]{2, 6, 3, 10};
 
         // создаю новый массив, обЪединяющий размеры лист1 илист 2
         int[] multiList = new int[list1.length + list2.length];
@@ -29,7 +28,10 @@ public class Solution2 {
         System.arraycopy(list1, 0, multiList, 0, list1.length);
 
         //копируем значения элементов лист2 в мультилист
-        System.arraycopy(list2, 0, multiList, 0, list2.length);
+        System.arraycopy(list2, 0, multiList, list1.length, list2.length);
+
+        Arrays.sort(multiList);
+
         return multiList;
 
     }
